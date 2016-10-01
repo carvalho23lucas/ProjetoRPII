5581bbc7acb6743a6b6d55a747d40da6f508b998
Lucas Carvalho <carvalho23lucas@gmail.com>
2016-10-01 00:00:45 -0300

    First Commit!

---
+++ Assets/Translator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+[System.Serializable]
+public class Translator {
+	Dictionary<int, string[]> grid = new Dictionary<int, string[]>();
+
+	public void AddToList(string item, int rowPos, int colPos){
+		if (grid [rowPos] == null)
+			grid [rowPos] = new string[10];
+		grid [rowPos] [colPos] = item;
+	}
+}
