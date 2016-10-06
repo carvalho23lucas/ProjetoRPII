cc51e1ff5631fd47f2a44c3564563b8488cca802
Lucas Carvalho <carvalho23lucas@gmail.com>
2016-10-06 17:45:59 -0300

    Grid v1 created!

---
+++ Assets/Scripts/MainGrid.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MainGrid : MonoBehaviour {
+	public int width, height, topstart;
+	[SerializeField] Transform ScrollContent;
+	[SerializeField] GameObject Cell;
+
+	GameObject[,] grid;
+	[SerializeField] GameObject SideMenu;
+
+	void OnGUI()
+	{
+		
+	}
+
+	void Start()
+	{
+		Component[] Buttons = SideMenu.GetComponentsInChildren<Button> ();
+		foreach (Component button in Buttons){
+			int buttonid = int.Parse((button as Button).name);
+			(button as Button).onClick.AddListener (() => { 
+				Translator.idbutton = buttonid;
+			});
+		}
+
+		grid = new GameObject[width, height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject cell = Instantiate (Cell);
+
+				cell.GetComponent<Button>().onClick.AddListener (() => {
+					if (Translator.idbutton == 2)
+						cell.GetComponent<RawImage>().texture = Resources.Load("PlayNote") as Texture;
+					else
+						cell.GetComponent<RawImage>().texture = Resources.Load("EmptyCell") as Texture;
+				});
+				cell.transform.position = new Vector2 (5 * x, 5 * y);
+				cell.transform.SetParent(ScrollContent, false);
+
+				grid [x, y] = cell;
+			}
+		}
+	}
+
+	void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+[System.Serializable]
+public static class Translator {
+	public static Dictionary<int, string[]> grid = new Dictionary<int, string[]>();
+	public static int idbutton;
+
+	public static void AddToList(string item, int rowPos, int colPos){
+		if (grid[rowPos] == null)
+			grid[rowPos] = new string[10];
+		grid[rowPos][colPos] = item;
+	}
+}
